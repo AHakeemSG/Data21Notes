7216c845f5f0e30d62b5527accf0d14696670fbd
AHakeemSG <82883718+AHakeemSG@users.noreply.github.com>
2021-04-20 17:23:54 +0100

    user input task for individual to go cinema inc name, age and which movies can be watched through loops

---
+++ user_input_task.py
@@ -0,0 +1,12 @@
+# Task for user input to for individual to go to the cinema
+name = input("What is your name? ")
+print(f"Hello,  {input}")
+
+age = input("How old are you? ")
+print(f"You are {input} ")
+
+movies_dict = {"movie names": ("The Longest Yard", "The Lion King", "Cinderella Story", "SAW IV"),
+               "movie ages": (15, 0, 12, 18)
+               "movie description": ("Ex-football star gone to prison", "The journey of a cub to redeem misfourunes", "Overcoming hardships to see true beauty", "Manic strikes again")}
+
+
