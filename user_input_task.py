5070f1fbcf0ef4bc593d329c5d16496ab7b3c814
AHakeemSG <82883718+AHakeemSG@users.noreply.github.com>
2021-04-21 15:32:02 +0100

    function notes

--- user_input_task.py
+++ user_input_task.py
@@ -1,12 +1,42 @@
 # Task for user input to for individual to go to the cinema
-name = input("What is your name? ")
-print(f"Hello,  {input}")
+age_prompt = True
+name_prompt = True
 
-age = input("How old are you? ")
-print(f"You are {input} ")
 
-movies_dict = {"movie names": ("The Longest Yard", "The Lion King", "Cinderella Story", "SAW IV"),
-               "movie ages": (15, 0, 12, 18)
-               "movie description": ("Ex-football star gone to prison", "The journey of a cub to redeem misfourunes", "Overcoming hardships to see true beauty", "Manic strikes again")}
+while name_prompt is True:
+    name = input(f"What is your name?  ".capitalize())
+    if name.isalpha():
+        name_prompt = False
+    else:
+        print("Please use alphabetic letters for your name!")
+
+while age_prompt is True:
+    age = input(f"How old are you? {name}?  ")
+    if age.isdigit():
+        age_prompt = False
+    else:
+        print(f"{name}, Please insert your age in digits  {input} ")
+
+films_to_watch = {
+    "u_rated": ["The Lion King", "Frozen", "Toy Story", "All Dogs Go To Heaven"],
+    "pg_rated": ["Cinderella Story", "The Princess Diaries", "Cheetah Girls", "Divergent"],
+    "15_rated": ["The Longest Yard", "Green Mile", "American Assassin", "Iron Man", "Batman VS Superman"],
+    "18_rated": ["Saw IV", "Freddy VS Jason", "Wolf of Wall Street", "Blade Trinity"]
+}
+
+while age_prompt is True:
+    if age >= 110:
+        print("Unfortunately, the age is not within a valid range.")
+    elif age >= 18:
+        print(f"You can watch a range of these movies ", {films_to_watch[:]})
+    elif age >= 15 < 18:
+        print(f"You can watch a range of these movies ", {films_to_watch[:3]})
+    elif age >= 12 < 15:
+        print(f"You can watch a range of these movies ", {films_to_watch[:2]})
+    else:
+        age <= 11
+        print(f"You can watch a range of these movies ", {films_to_watch[:1]})
+    break
+
 
 
---
+++ venv/functions.py
@@ -0,0 +1,33 @@
+# # Functions
+
+# # Function 1
+# def greeting(name: str):  # specifies that the argument expects to be string
+#     print(f"Hello {name}")
+#
+#
+# greeting("Abanoub")
+
+# # Function 2
+# def addition(int1, int2):
+#     return int1 + int2
+#
+#
+# print(addition(2, 3))
+
+
+# # Function 3
+# def division(int3, int4: int) -> float:  # tells us what data type we should expect
+#     return int3 / int4
+#
+#
+# print(division(8, 2))
+
+
+# # How to add expected default in arguements
+# def divison(int3=8, int4=2):
+#     return int3 / int4
+#
+#
+# print(division())
+
+from numbers import num_list
